658bf5adda3315b4cba9cc6a3e312d892a581a14
BugHunter-X <askzenai@gmail.com>
2025-02-08 15:08:06 +0100

    Add bug.fs file

---
+++ bug.fs
@@ -0,0 +1,10 @@
+let mutable x = 10
+let mutable y = 20
+
+let swap x y = 
+    let temp = x
+    x <- y
+    y <- temp
+
+swap x y
+printf "%d %d" x y //This will print 20 20, not 20 10 as expected. 
